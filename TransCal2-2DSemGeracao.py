387daed2acdafe4fa5be4711c7042508dcf4ca1f
lucasmmiran <58879231+lucasmmiran@users.noreply.github.com>
2022-05-16 19:38:25 -0300

    Add files via upload

---
+++ TransCal2-2DSemGeracao.py
@@ -0,0 +1,104 @@
+# Autor: Lucas Mendes Miranda
+#  Data: 13/05/2022
+
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+# parametros da simulacao
+alpha = 1.0
+
+# parametros da malha
+Lx = 1.0
+Ly = 1.0
+nx = 14
+ny = 20
+npoints = nx*ny
+ne = (nx-1)*(ny-1)
+dx = Lx/(nx-1)
+dy = Ly/(ny-1)
+
+# Formando os pontos da malha em X e Y
+Xv = np.linspace(0, Lx, nx )
+Yv = np.linspace(0, Ly, ny )
+
+# Criando o Grid
+X, Y =np.meshgrid(Xv, Yv)
+
+#Colocando no formato desejado (Transformando a matriz para um vetor)
+X = np.reshape(X, npoints)
+Y = np.reshape(Y, npoints)
+
+
+# Criando os vetores de CC
+cc1, cc2, cc3, cc4  = [], [], [], []
+
+# Preenchendo os vetores 1 e 3
+for i in range(0, nx):
+    cc1 = cc1 + [i]
+    cc3 = cc3 + [(npoints - 1 - i)]
+
+# Preenchendo os vetores 2 e 4
+for i in range(0, ny-2):
+    cc2 = cc2 + [((i+2)*nx)-1]
+    cc4 = cc4 + [(i+1)*nx]
+
+cc = cc1 + cc2 + cc3 + cc4 
+
+print('Meus CC:')
+print(cc)
+
+#Vetor de condições de contorno
+Tcc = np.zeros( (npoints), dtype = 'float')
+
+# Preenchendo o vetor de CCs
+#CC1
+for i in cc1:
+    Tcc[i] = X[i]
+
+#CC2
+for i in cc2:
+    Tcc[i] = Y[i]*Y[i] + 1
+
+#CC3
+for i in cc3:
+    Tcc[i] = X[i]*X[i] + 1
+
+#CC4
+for i in cc4:
+    Tcc[i] = Y[i]
+
+# Montando a diagonal dos valores de contorno
+A = np.zeros( (npoints, npoints), dtype = 'float')
+b = np.zeros( (npoints), dtype = 'float')
+
+for i in cc:
+    A[i,i] = 1.0
+    b[i] = Tcc[i]
+
+# Montando o Miolo
+
+for eq in range(0, npoints-1):
+    if not eq in cc:
+        A[eq,eq+1] = alpha/(dx*dx)
+        A[eq,eq] = -2*alpha/(dx*dx) - 2*alpha/(dy*dy)
+        A[eq,eq-1] = alpha/(dx*dx)
+        A[eq,eq + nx] = alpha/(dy*dy)
+        A[eq,eq  - nx] = alpha/(dy*dy)
+
+Ainv = np.linalg.inv(A)
+T = Ainv@b
+
+print('Meu T:')
+print(T)
+
+Z = T.reshape(ny,nx)
+surf = plt.imshow(Z, interpolation='quadric', origin='lower',
+                cmap=cm.jet, extent=(X.min(), X.max(), Y.min(), Y.max()))
+plt.colorbar(surf,shrink=1.0, aspect=20)
+plt.grid(color='black', linestyle='solid', linewidth=0.5)
+labx = np.linspace(X.min(),X.max(),nx)
+laby = np.linspace(Y.min(),Y.max(),ny)
+plt.xticks(labx)
+plt.yticks(laby)
+plt.show()
